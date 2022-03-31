e503ccb3ae6b9fd451eed7c3a7f066d9a208957b
Rogrback <rfzt96@gmail.com>
2022-03-31 01:26:16 -0500

    First commit

---
+++ src/bases/8-imp-exp.js
@@ -0,0 +1,16 @@
+// import heroes, { owners } from '../data/heroes';
+
+// // console.log(heroes);
+
+// console.log(owners);
+
+// const getHeroeById= (id)=> heroes.find((heroe)=> heroe.id === id);
+
+
+// console.log(getHeroeById(3));
+
+// const getHeroesByOwner= (owner)=> heroes.filter((heroe)=> heroe.owner === owner);
+
+// // const {owner} = getHeroesByOwner(heroes)
+
+// console.log(getHeroesByOwner('DC'));
--- src/index.js
+++ src/index.js
@@ -1,17 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import heroes from './data/heroes';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// console.log(heroes);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// console.log(owners);
+
+const getHeroeById= (name)=> heroes.find((heroe)=> heroe.name === name);
+
+const {name} = getHeroeById('Batman')
+
+
+console.log(name);
+
+const getHeroesByOwner= (owner)=> heroes.filter((heroe)=> heroe.owner === owner);
+
+const [arr] = getHeroesByOwner('Marvel')
+
+const {owner}= arr 
+
+console.log(owner);
+
+// const getHeroesByOwner2= (owner2)=>heroes.filter((heroe)=> heroe.owner === owner2);
+
+// const [owner2] = getHeroesByOwner2('Marvel')
+
+// console.log(owner2);
